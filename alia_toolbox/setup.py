a96883bfa4c0982281eb55e0172d5d4682282fc2
Alia <alia.jo.victor@gmail.com>
2020-12-22 20:47:18 -0500

    Added more details to setup.py

--- alia_toolbox/setup.py
+++ alia_toolbox/setup.py
@@ -4,8 +4,10 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='alia_toolbox',
-      version='0.0.1',
-      url='#',
+      version='0.0.2',
+      author='Alia Victor',
+      author_email='alia.jo.victor@gmail.com',
+      url='https://github.com/aliavictor/portfolio/tree/main/alia_toolbox',
       packages=['alia_toolbox'],
       include_package_data=True,
       install_requires=requirements,
