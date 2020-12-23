d123b9dad411a3332f0b8656141ad66b2c4a981d
Alia <alia.jo.victor@gmail.com>
2020-12-22 21:05:25 -0500

    Added more info to setup.py.

--- bleach_search/setup.py
+++ bleach_search/setup.py
@@ -5,7 +5,9 @@ with open('requirements.txt') as f:
 
 setup(name='bleach_search',
       version='0.0.1',
-      url='#',
+      author='Alia Victor',
+      author_email='alia.jo.victor@gmail.com',
+      url='https://github.com/aliavictor/portfolio/tree/master/bleach_search',
       packages=['bleach_search'],
       include_package_data=True,
       install_requires=requirements,
